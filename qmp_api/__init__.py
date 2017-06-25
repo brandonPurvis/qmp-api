77ca395c3bf1323bcb74fc3ac99a703b22ca731d
Brandon <bcpmax@gmail.com>
2017-06-25 16:43:32 -0400

    Separate settings into independant module

--- qmp_api/__init__.py
+++ qmp_api/__init__.py
@@ -22,17 +22,9 @@ def make_celery(app):
     return celery
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.update(dict(
-    YOUTUBE_DATA_API=os.environ['YOUTUBE_DATA_API'],
-    SQLALCHEMY_DATABASE_URI='sqlite:////tmp/qmp_api.db',
-	CELERY_BROKER_URL='pyamqp://guest@localhost//',
-	CELERY_RESULT_BACKEND='',
-	LIKE_PLAYLIST_ID='LLjYn4RyjCCMJX67Rck4sq5A',
-	SPEC_CHANNEL_PLAYLIST_ID='PLa0DseGKotuAHBwFnxFEsU5qGL9F3fxe4',
-	SPEC_VIDEO_PLAYLIST_ID='PLa0DseGKotuA_ciqFFvpYr7hBsv3z8LLl',
-	ROOT_CHANNEL_ID='UCjYn4RyjCCMJX67Rck4sq5A',
-))
+app.config.from_object('qmp_api.settings')
+app.config.from_object('qmp_api.secrets')
+
 db = SQLAlchemy(app)
 celery = make_celery(app)
 
